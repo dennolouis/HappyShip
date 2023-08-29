c52d07e3d5d898b70fbfc61922e6e1759db2ac14
Denno Louis <dennolouis@Dennos-Air-2.fios-router.home>
2023-08-29 10:38:33 -0400

    bunny animations in bri level

---
+++ HappyShip/Assets/Scripts/BunnySpawn.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnySpawn : MonoBehaviour
+{
+    [SerializeField] GameObject bunnyPrefab;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // Create a rotation that represents a 90-degree rotation around the y-axis.
+            Quaternion rotation = Quaternion.Euler(0f, -90f, 0f);
+
+            // Instantiate the bunny object with the specified rotation.
+            GameObject bunny = Instantiate(bunnyPrefab, transform.position, rotation);
+
+            bunny.transform.localScale += new Vector3(2, 2, 2);
+
+
+            Animator anim = bunny.GetComponent<Animator>();
+
+            anim.SetInteger("AnimIndex", 1);
+            anim.SetTrigger("Next");
+
+
+            Destroy(bunny, 10f);
+
+        }
+    }
+}
