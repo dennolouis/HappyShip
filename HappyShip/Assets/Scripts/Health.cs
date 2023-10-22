1f31733bed8380704dfc3c80ad999e5e9366dd5b
Denno Louis <dennolouis@Dennos-Air-2.fios-router.home>
2023-10-22 19:00:54 -0400

    destroy effect on health script

--- HappyShip/Assets/Scripts/Health.cs
+++ HappyShip/Assets/Scripts/Health.cs
@@ -6,12 +6,16 @@ public class Health : MonoBehaviour
 {
     [SerializeField] int health = 5;
 
+    [SerializeField] GameObject destoryEffect;
+    [SerializeField] float destroyEffectDuration = 1f;
+
     void GetHit()
     {
         health--;
 
         if(health <= 0)
         {
+            ShowDestoryEffect();
             Destroy(gameObject);
         }
     }
@@ -20,9 +24,20 @@ public class Health : MonoBehaviour
     {
         if (collision.gameObject.tag == "Projectile")
         {
-            Destroy(collision.gameObject);
             GetHit();
+            collision.gameObject.GetComponent<Projectile>().ShowHitEffect();
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void ShowDestoryEffect()
+    {
+        if(destoryEffect)
+        {
+            GameObject effect = Instantiate(destoryEffect, transform.position, Quaternion.identity);
+            Destroy(effect, destroyEffectDuration);
         }
 
     }
+
 }
---
+++ HappyShip/Assets/Scripts/Projectile.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] GameObject hitEffect;
+
+    public void ShowHitEffect()
+    {
+        if (hitEffect)
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.5f);
+        }
+    }
+}
