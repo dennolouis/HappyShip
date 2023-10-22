d9117d56e426deb132abc1d7db190af28985ed38
Denno Louis <dennolouis@Dennos-Air-2.fios-router.home>
2023-10-22 00:13:43 -0400

    IOS notifications work

--- HappyShip/Assets/Scripts/Notifications/IOSNotifications.cs
+++ HappyShip/Assets/Scripts/Notifications/IOSNotifications.cs
@@ -1,32 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+#if UNITY_ANDROID
+using Unity.Notifications.Android;
+using UnityEngine.Android;
+#elif UNITY_IOS
 using Unity.Notifications.iOS;
+#endif
+
 using UnityEngine;
 
 public class IOSNotifications : MonoBehaviour
 {
+    private int hours = 120; //5 days
+    private int minutes;
+    private int seconds;
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(RequestAuthorization());
+#if UNITY_IOS
+        SendIOSNotification();
+#elif UNITY_ANDROID
+        SendAndroidNotification();
+#endif
     }
 
-    IEnumerator RequestAuthorization()
+#if UNITY_IOS
+    void SendIOSNotification()
     {
-        var authorizationOption = Unity.Notifications.iOS.AuthorizationOption.Alert | AuthorizationOption.Badge;
-        using (var req = new AuthorizationRequest(authorizationOption, true))
+        var timeTrigger = new iOSNotificationTimeIntervalTrigger()
         {
-            while (!req.IsFinished)
-            {
-                yield return null;
-            };
+            TimeInterval = new TimeSpan(hours, minutes, seconds),
+            Repeats = false
+        };
 
-            string res = "\n RequestAuthorization:";
-            res += "\n finished: " + req.IsFinished;
-            res += "\n granted :  " + req.Granted;
-            res += "\n error:  " + req.Error;
-            res += "\n deviceToken:  " + req.DeviceToken;
-            Debug.Log(res);
-        }
+        var notification = new iOSNotification()
+        {
+            // You can specify a custom identifier which can be used to manage the notification later.
+            // If you don't provide one, a unique string will be generated automatically.
+            Identifier = "_notification_01",
+            Title = "Happy Ship",
+            Body = "",
+            Subtitle = "Come Back!",
+            ShowInForeground = true,
+            ForegroundPresentationOption = (PresentationOption.Alert | PresentationOption.Sound),
+            CategoryIdentifier = "category_a",
+            ThreadIdentifier = "thread1",
+            Trigger = timeTrigger,
+        };
+
+        //make sure the notification does display too much
+        iOSNotificationCenter.RemoveScheduledNotification(notification.Identifier);
+
+        iOSNotificationCenter.ScheduleNotification(notification);
     }
+#elif UNITY_ANDROID
+    void SendAndroidNotification()
+    {
+        if (!Permission.HasUserAuthorizedPermission("android.permission.POST_NOTIFICATIONS"))
+        {
+            Permission.RequestUserPermission("android.permission.POST_NOTIFICATIONS");
+        }
+
+
+        var notification = new AndroidNotification();
+        notification.Title = "Happy Ship";
+        notification.Text = "Come Back!";
+        notification.FireTime = System.DateTime.Now.AddSeconds(10);
+
+        AndroidNotificationCenter.SendNotification(notification, "channel_id");
+    }
+#endif
 }
