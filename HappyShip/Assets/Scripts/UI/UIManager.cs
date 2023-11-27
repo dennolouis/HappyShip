fc6624fa47b63455aa5db77cd51227d2f4e8bee7
ClarenceE3 <90079900+ClarenceE3@users.noreply.github.com>
2023-11-27 00:47:25 -0500

    Task has been completed... Took me long enough smh
    Pages slide in... feel like there's a more efficient way to do this.

--- HappyShip/Assets/Scripts/UI/UIManager.cs
+++ HappyShip/Assets/Scripts/UI/UIManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public RectTransform mainMenu, levelSelect, settingSelect;
+
+    //This tells the main menu to slide into the camera
     void Start()
     {
-        
+        mainMenu.DOAnchorPos(new Vector2(400,300), 0.25f);
     }
 
-    // Update is called once per frame
-    void Update()
+    //This is for the Levels Canvas to slide in... Pretty sure there's a better way to do this...
+    public void LevelSelectButton()
     {
-        
+        mainMenu.DOAnchorPos(new Vector2(-400,300),0.25f);
+        levelSelect.DOAnchorPos(new Vector2(400,300),0.25f);
+        settingSelect.DOAnchorPos(new Vector2(-1200,300),0.25f);
     }
+    public void CloseLevelSelectButton()
+    {
+        mainMenu.DOAnchorPos(new Vector2(400, 300), 0.25f);
+        levelSelect.DOAnchorPos(new Vector2(1200,300),0.25f);
+        settingSelect.DOAnchorPos(new Vector2(-400,300),0.25f);
+    }
+    //This allows Settings Canvas to slide in
+    public void SettingSelectButton()
+    {
+        mainMenu.DOAnchorPos(new Vector2(1200, 300), 0.25f);
+        levelSelect.DOAnchorPos(new Vector2(2000,300),0.25f);
+        settingSelect.DOAnchorPos(new Vector2(400,300),0.25f);
+    }
+    public void CloseSettingSelectButton()
+    {
+        mainMenu.DOAnchorPos(new Vector2(400, 300), 0.25f);
+        levelSelect.DOAnchorPos(new Vector2(1200,300),0.25f);
+        settingSelect.DOAnchorPos(new Vector2(-400,300),0.25f);
+    }
+
+
+
 }
