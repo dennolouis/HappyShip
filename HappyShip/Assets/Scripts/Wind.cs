ac7ebe638f8be2c18109a4bd38a40503b549f9df
Denno Louis <dennolouis@Dennos-Air-2.fios-router.home>
2023-11-28 16:28:14 -0500

    logic for wind prefab

---
+++ HappyShip/Assets/Scripts/Wind.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    // Adjust this value to control the strength of the upward force
+    [SerializeField] float force = 50f;
+
+    float y;
+
+    private void Start()
+    {
+        y = GetComponent<Renderer>().material.mainTextureOffset.y;
+    }
+
+
+    private void Update()
+    {
+        y += force / 40 * Time.deltaTime;
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(0, y));
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // Check if the entering object has the "Player" tag
+        if (other.CompareTag("Player"))
+        {
+            // Get the Rigidbody component of the player
+            Rigidbody playerRigidbody = other.GetComponent<Rigidbody>();
+
+            // Check if the Rigidbody component is not null
+            if (playerRigidbody != null)
+            {
+                // Get the upward direction of the wind (Y direction of the GameObject)
+                Vector3 windDirection = transform.up;
+
+                // Apply an upward force to the player's Rigidbody in the wind direction
+                playerRigidbody.AddForce(windDirection * force * Time.deltaTime, ForceMode.Impulse);
+            }
+        }
+    }
+}
